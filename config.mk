bf4584712fbea01b7bf97d09ecde8ab8ff6342b5
mydongistiny <jaysonedson@gmail.com>
2016-10-15 16:25:56 -0700

    addons: Remove Google Messenger to use AOSP version
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- config.mk
+++ config.mk
@@ -1,14 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
-# Substratum. Use recreate
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.substratum.recreate=true
-
 # Prebuilts
 PRODUCT_PACKAGES += \
     BenzoBlock \
     KernelAdiutor \
-    PrebuiltBugle \
     SnapBrowser \
     Substratum
